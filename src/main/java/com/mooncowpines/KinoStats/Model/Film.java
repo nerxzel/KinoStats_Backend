ba9ba55e548abb4251744bd11b69e5778f7202e4
Alfo <alf.luna@duocuc.cl>
2026-05-09 12:54:50 -0400

    Log dto y findbyemail

--- src/main/java/com/mooncowpines/KinoStats/Model/Film.java
+++ src/main/java/com/mooncowpines/KinoStats/Model/Film.java
@@ -7,8 +7,6 @@ import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
